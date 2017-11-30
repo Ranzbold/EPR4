103392f54a2ffbf8bae59e3c997cf5cabb672b91
fabzen <33436717+fabzen@users.noreply.github.com>
2017-11-30 15:03:47 +0100

    Update elevator_cmd_computing.py
    da ist jetzt noch etwas mehr implementiert. jetzt muesste man testen, denke ich

--- elevator_cmd_computing.py
+++ elevator_cmd_computing.py
@@ -1,55 +1,90 @@
+import random
 from elevator_gui import update_gui
 
 def calculate_priority(cmd_elevator, cmd_floor, current_state, last_state):
+    
     int_elevator = []
     int_floor = []
 
-
+    #transforming to integers
     for i in range(len(cmd_elevator)):
         int_elevator.append(int(cmd_elevator[i][1]))   
     for i in range(len(cmd_floor)):
         int_floor.append(int(cmd_floor[i][0])) 
 
+    #calculate priority destination for elevator
     if not cmd_elevator:
         if cmd_floor:
-            next_cmd = cmd_floor[0]
-            return next_cmd
+            next_cmd = int_floor[0]
+            cmd_floor.pop(0)
         else:
-            return 
+            next_cmd = current_state
     else:
         if not (1 < current_state < 6):
-            next_cmd = cmd_elevator[0]
-            return next_cmd
+            next_cmd = int_elevator[0]
         else:
             direction = last_state - current_state
-            valid_cmd = [0] * len(cmd_elevator)
+            valid_cmd = [0] * len(int_elevator)
             if direction < 0:
-                for x in range(len(cmd_elevator)):
+                for x in range(len(int_elevator)):
                     if int_elevator[x] > current_state:
                         valid_cmd[x] = 1
-                try:
-                    next_cmd_idx = [idx for idx in range(len(cmd_elevator)) if valid_cmd[idx] == 1][0]
-                    next_cmd = cmd_elevator[next_cmd_idx]
-                    return next_cmd
-                except sum(valid_cmd) == 0:
-                    next_cmd = cmd_elevator[0]
-                    return next_cmd
-                    
+                #check whether there's a target in the current direction
+                if sum(valid_cmd) == 0:
+                    next_cmd = int_elevator[0]  
+                else:
+                    next_cmd_idx = [idx for idx in range(len(int_elevator)) \
+                                    if valid_cmd[idx] == 1][0]
+                    next_cmd = int_elevator[next_cmd_idx] 
             elif direction > 0:
-                for x in range(len(cmd_elevator)):
+                for x in range(len(int_elevator)):
                     if int_elevator[x] < current_state:
                         valid_cmd[x] = 1
-                try:
-                    next_cmd_idx = [idx for idx in range(len(cmd_elevator)) if valid_cmd[idx] == 1][0]
-                    next_cmd = cmd_elevator[next_cmd_idx]
-                    return next_cmd
-                except sum(valid_cmd) == 0:
-                    next_cmd = cmd_elevator[0]
-                    return next_cmd                  
+                #check whether there's a target in the current direction
+                if sum(valid_cmd) == 0:
+                    next_cmd = int_elevator[0] 
+                else:
+                    next_cmd_idx = [idx for idx in range(len(int_elevator)) \
+                                    if valid_cmd[idx] == 1][0]
+                    next_cmd = int_elevator[next_cmd_idx]
+                
+                   
             else:
-                next_cmd = cmd_elevator[0]
-                return next_cmd
+                next_cmd = int_elevator[0]
+                
+    #determine movement of elevator
+    if next_cmd > current_state:
+        next_step = current_state + 1
+    elif next_cmd < current_state:
+        next_step = current_state - 1
+    elif next_cmd == current_state:
+        next_step = current_state
+    return next_step, cmd_floor
 
+
+
+def halt(next_step, cmd_elevator, cmd_floor):
+    #check whether people want to get in/out at current floor
+    coincidence = False
+    for i in range(len(cmd_elevator)):
+        if str(next_step) in cmd_elevator[i]:
+            cmd_elevator.pop(i)
+            coincidence = True
+            break
+        
+    for i in range(len(cmd_floor)):
+        if str(next_step) in cmd_floor[i]:
+            cmd_floor.pop(i)
+            coincidence = True
+            break
+            
+    if coincidence == True:
+        wait_parameter = random.randint(1,4)
+    else:
+        wait_parameter = 0
+    
+    return wait_parameter, cmd_elevator, cmd_floor
+    
 
 
 
@@ -60,50 +95,63 @@ last_state = [2, 2]
 cmd_floor = []
 cmd_A = []
 cmd_B = []
+wait_A = 0
+wait_B = 0
 
 
-usrinput = input('--> ')
-commands = usrinput.split(' ')
-#evaluate correctness of entry data
-
-
-if commands[0] != '':
-    for cnt in range(len(commands)):
-        if  'r' in commands[cnt][1] or 'h' in commands[cnt][1]:
-            dict_cmd_floor = dictionary[commands[cnt][0]]
-            commands[cnt] = dict_cmd_floor + commands[cnt][1]
-            cmd_floor.append(commands[cnt])
-        elif ('A') in commands[cnt][0]:
-            dict_cmd_elevator = dictionary[commands[cnt][1]]
-            commands[cnt] = commands[cnt][0] + dict_cmd_elevator
-            cmd_A.append(commands[cnt])
-        elif ('B') in commands[cnt][0]:
-            dict_cmd_elevator = dictionary[commands[cnt][1]]
-            commands[cnt] = commands[cnt][0] + dict_cmd_elevator
-            cmd_B.append(commands[cnt])
-
-
-
-#implement waiting of 1-3 rounds
+for turn in range(10):
     
-#make a loop with recursive calling of functions and updating of last_state, current_state
-if cmd_A:
-    next_A = calculate_priority(cmd_A, cmd_floor, current_state[0], last_state[0])
-    if not(next_A):
-        next_A = 'A' + str(current_state[0]) 
-    #pop task from A and B
-if cmd_B:
-    next_B = calculate_priority(cmd_B, cmd_floor, current_state[1], last_state[1])
-    if not(next_B):
-        next_B = 'B' + str(current_state[1])
-    #pop task from A and B
-
-try:
-    gui_A = int(next_A[0])
-    gui_B = int(next_B[0])    
-except:
-    gui_A = int(next_A[1])
-    gui_B = int(next_B[1])
+    usrinput = input('--> ')
+    commands = usrinput.split(' ')
+    #evaluate correctness of entry data
     
-update_gui(gui_A, gui_B)
-
+    
+    if commands[0] != '':
+        for cnt in range(len(commands)):
+            #distribute commands among lists and convert them from K-4 to 1-6
+            if  'r' in commands[cnt][1] or 'h' in commands[cnt][1]:
+                dict_cmd_floor = dictionary[commands[cnt][0]]
+                commands[cnt] = dict_cmd_floor + commands[cnt][1]
+                if commands[cnt] not in cmd_floor:
+                    cmd_floor.append(commands[cnt])
+            elif ('A') in commands[cnt][0]:
+                dict_cmd_elevator = dictionary[commands[cnt][1]]
+                commands[cnt] = commands[cnt][0] + dict_cmd_elevator
+                if commands[cnt] not in cmd_A:
+                    cmd_A.append(commands[cnt])
+            elif ('B') in commands[cnt][0]:
+                dict_cmd_elevator = dictionary[commands[cnt][1]]
+                commands[cnt] = commands[cnt][0] + dict_cmd_elevator
+                if commands[cnt] not in cmd_B:
+                    cmd_B.append(commands[cnt])
+    
+    
+    if wait_A == 0:
+        [next_A, cmd_floor] = calculate_priority(cmd_A, cmd_floor, current_state[0], last_state[0])
+    else:
+        next_A = current_state[0]
+        
+    [wait_A, cmd_A, cmd_floor] = halt(next_A, cmd_A, cmd_floor)
+        
+        
+    if wait_B == 0:
+        [next_B, cmd_floor] = calculate_priority(cmd_B, cmd_floor, current_state[1], last_state[1])
+    else:
+        next_B = current_state[1]
+        
+    [wait_B, cmd_B, cmd_floor] = halt(next_B, cmd_B, cmd_floor)
+    
+    
+    update_gui(next_A, next_B)
+    
+    #counter for elevator entry/exit
+    if wait_A != 0:
+        wait_A -= 1
+    if wait_B != 0:
+        wait_B -= 1
+        
+    #update state
+    last_state = current_state
+    current_state = [next_A, next_B]
+    
+    print('cmd_A', cmd_A, 'cmd_B', cmd_B, 'cmd_floor', cmd_floor, 'current_state', current_state)
